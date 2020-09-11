c758696d54b7d525a8b92b28ca6820357a2880d8
Max Lapides <max.lapides@tonal.com>
2020-09-11 16:58:35 -0700

    more updates

--- src/components/sections/TOC.js
+++ src/components/sections/TOC.js
@@ -4,7 +4,7 @@ import Section from "../Section";
 import Container from "../Container";
 import Index from "../Index";
 import NavAnchor from "../NavAnchor";
-import { H2, H3, P } from "../typography";
+import { H2, H3, P, UL, ULI } from "../typography";
 import Link from "../Link";
 
 const Spacer = styled.div`
@@ -27,27 +27,41 @@ class TOC extends Component {
               }}
             >
               <H2>Updates</H2>
-
+              <H3>Zoom Info</H3>
               <P>
-                <strong>
-                  Read our full agenda and Priorities Proposals in the
-                  <Link href="https://docs.google.com/document/d/1eLqZoP15qcFSB8RTuICvywExbNy7J2PX1a3bEhWrEVw/edit#">
-                    Convention Packet
-                  </Link>
-                  .
-                </strong>{" "}
-                This packet has all the information you’ll need to know for our
-                Convention day-of.
+                <Link href="https://us02web.zoom.us/j/88197589947?pwd=eDJvajZYdUF1VlRxd1loRGI0QThBdz09">
+                  Zoom link
+                </Link>
+                <br />
+                Meeting ID: 881 9758 9947
+                <br />
+                Passcode: 215458
               </P>
-
+              <H3>Voting</H3>
               <P>
-                We’re so excited to announce our full panel on the topic of “How
-                Do Socialists Build Power?” We’ll be welcoming socialist
-                all-stars Cecily Myart-Cruz (President of UTLA), Michael Lighty
-                (national leader in the fight for M4A), and Jabari Brisport
-                (soon-to-be State Senator for New York’s 25th). All three of
-                them will have a lot to share on this panel moderated by our
-                co-chairs and with time for a brief Q&amp;A.
+                In order to sign in to the software we’ll be using to vote and
+                motion all you need is the email address that you signed up for
+                DSA with. Make sure that you’re a dues-paying DSA member and if
+                you have any technical issues during the meeting, you can email
+                info@eastbaydsa.org.
+              </P>
+              <P>
+                <UL>
+                  <ULI>
+                    <Link href="https://vote.phillydsa.org/event/east-bay-convention">
+                      Click here
+                    </Link>
+                    to get started.
+                  </ULI>
+                  <ULI>
+                    Sign in with your email to get sent a link and then close
+                    the tab. Getting sent the link may take a second.
+                  </ULI>
+                  <ULI>
+                    Open the new link you got in your inbox and you’re logged
+                    in!
+                  </ULI>
+                </UL>
               </P>
             </div>
 
